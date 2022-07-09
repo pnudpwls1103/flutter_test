671734b9c9596c87468ea85683caa5ef25134f51
민예진 <pnudpwls1103@pusan.ac.kr>
2022-07-09 22:05:56 +0900

    udemy ~177강 : Drawer 설계

--- meal_app/lib/screens/tabs_screen.dart
+++ meal_app/lib/screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 import '../screens/favorites_screen.dart';
 import '../screens/categories_screen.dart';
 
@@ -34,6 +35,7 @@ class _TabScreenState extends State<TabScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
